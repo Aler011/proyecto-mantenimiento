9c1860e3d818e865541acfca1dbf44d66172d36f
Alexis Rosaldo <alexisrosaldo@gmail.com>
2024-05-08 00:26:07 -0600

    Añadiendo adaptacion a dispositivos moviles en las tarjetas de noticias a botones, imagenes y tamaño

--- versiones-proyecto/application_v1/views/feed.php
+++ versiones-proyecto/application_v1/views/feed.php
@@ -9,7 +9,6 @@
 		<!-- Favicon-->
 		<link rel="icon" type="image/x-icon" href="../assets/favicon.ico" />
 		<!-- Core theme CSS (includes Bootstrap)-->
-		<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
 		<link href="../css/styles.css" rel="stylesheet" />
 		<link href="../css/styles_front.css" rel="stylesheet" />
 		<script type="text/javascript">
